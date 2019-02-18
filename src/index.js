c75ab0eb5a513f24df99a4075f3240927ac7b7c7
uniorunr <gagael23@live.com>
2019-02-18 23:14:38 +0300

    style: remove comments

--- src/index.js
+++ src/index.js
@@ -3,24 +3,24 @@ module.exports = function check(str, bracketsConfig) {
   const openingBr = [];
   const closingBr = [];
 
-  bracketsConfig.map(item => {  // creating arrays of opening and closing brackets
+  bracketsConfig.map(item => {
     openingBr.push(item[0]);
     closingBr.push(item[1]);
   });
 
-  for (let i = 1; i < str.length; i++) {      // checking of every char in string
+  for (let i = 1; i < str.length; i++) {
     let last = stack[stack.length - 1];
     if (last === str[i] && 
-      closingBr.indexOf(last) === openingBr.indexOf(last)) {      // checking if opening and closing bracket is same 
-      stack.pop();      // removing last element from the stack if brackets are balanced
-    } else if (openingBr.indexOf(str[i]) !== -1) {      // checking if the current char is the opening bracket
-      stack.push(str[i]);     // adding the current char to the stack
-    } else if (closingBr.indexOf(str[i]) === openingBr.indexOf(last)) {     // checking if the last element from the stack and current char are balanced
-      stack.pop();      // removing the last element from the stack if brackets are balanced
+      closingBr.indexOf(last) === openingBr.indexOf(last)) {
+      stack.pop();
+    } else if (openingBr.indexOf(str[i]) !== -1) {
+      stack.push(str[i]);
+    } else if (closingBr.indexOf(str[i]) === openingBr.indexOf(last)) {
+      stack.pop();
     } else return false;
   }
   
-  if (!stack.length) {      // checking the length of the stack
+  if (!stack.length) {
     return true;
   } return false;
 }
